455ca0b4be59570ab35d4887c1433dd6a6bea383
Nino Floris <mail@ninofloris.com>
2020-02-02 20:40:10 +0100

    Initial commit

---
+++ Lib/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lib
+{
+    public class Foo
+    {
+        public static ValueTuple<Type, Type>[] Bar()
+            => new[] { (typeof(int), typeof(int)) };
+
+        public static IEnumerable<ValueTuple<Type, Type>> WorkingBar()
+            => new[] { (typeof(int), typeof(int)) };
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Error()
+        {
+            foreach (var (x, y) in Lib.Foo.Bar()) { }
+        }
+
+        static ValueTuple<Type, Type>[] HelloSameAssembly()
+            => new[] { (typeof(int), typeof(int)) };
+
+        static void Success()
+        {
+            foreach (var (x, y) in HelloSameAssembly()) { }
+            foreach (var (x, y) in Lib.Foo.WorkingBar()) { }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
